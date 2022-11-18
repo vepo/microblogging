88bd86662f6da43c8589f1a00529ade2d7321279
Victor Osório <vepo@tutanota.com>
2022-11-18 17:57:01 -0300

    #1 Starting development

--- src/test/java/io/vepo/microblogging/post/PostEndpointTest.java
+++ src/test/java/io/vepo/microblogging/post/PostEndpointTest.java
@@ -1,14 +1,16 @@
 package io.vepo.microblogging.post;
 
 import static io.restassured.RestAssured.given;
+import static java.util.Collections.emptyList;
+import static org.hamcrest.Matchers.matchesPattern;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Collections;
+import java.util.UUID;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
@@ -21,29 +23,58 @@ public class PostEndpointTest {
 
     @Test
     @Order(1)
-    @DisplayName("Listing no Posts")
+    @DisplayName("Listing all Posts")
     public void testPostList() {
         var response = given().when()
                 .get("/post")
                 .thenReturn();
         assertEquals(response.statusCode(), 200);
         var body = response.jsonPath();
-        assertEquals(body.getList(".", Post.class), Collections.emptyList());
+        assertEquals(body.getList(".", Post.class), emptyList());
     }
 
     @Test
     @Order(2)
-    @DisplayName("Listing no Posts")
+    @DisplayName("Create Post")
     public void createPost() {
-        given().contentType(ContentType.JSON).body(new CreatePostRequest("Test")).when().post("/post").then()
-                .statusCode(201).header("Location", Matchers.matchesPattern(".*/post/[0-9]+"));
+        var uuid = UUID.randomUUID().toString();
+        given().contentType(ContentType.JSON)
+                .body(new CreatePostRequest("Test" + uuid))
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(201)
+                .header("Location", matchesPattern(".*/post/[0-9]+"));
         var response = given().when()
                 .get("/post")
                 .thenReturn();
         assertEquals(response.statusCode(), 200);
-        var body = response.jsonPath().getList(".", Post.class);
+        var body = response.jsonPath()
+                .getList(".", Post.class);
         assertEquals(1, body.size());
-        assertEquals("Test", body.get(0).getContent());
+        assertEquals("Test" + uuid, body.get(0).getContent());
+        assertTrue(body.get(0).getId() > 0);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Delete Post")
+    public void deletePost() {
+        var uuid = UUID.randomUUID().toString();
+        var post = given().contentType(ContentType.JSON)
+                .body(new CreatePostRequest("Test" + uuid))
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(201)
+                .header("Location", matchesPattern(".*/post/[0-9]+")).extract().as(Post.class);
+        given().when()
+                .delete("/post/" + post.getId())
+                .then().statusCode(200);
+        var response = given().when()
+                .get("/post")
+                .thenReturn();
+
     }
 
 }
