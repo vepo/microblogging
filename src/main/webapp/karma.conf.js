8d44c77c1d30bd8d6afb90511b7bb5276c586da1
Victor Osorio <victoros@amdocs.com>
2022-11-23 17:17:31 -0300

    Configuring TS Code Coverage

--- src/main/webapp/karma.conf.js
+++ src/main/webapp/karma.conf.js
@@ -1,16 +1,38 @@
-coverageReporter: {
-    dir: require('path').join(__dirname, './coverage/<project-name>'),
-    subdir: '.',
-    reporters: [
-      { type: 'html' },
-      { type: 'text-summary' }
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
-    check: {
-      global: {
-        statements: 80,
-        branches: 80,
-        functions: 80,
-        lines: 80
-      }
-    }
-  }
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      type : 'lcovonly',
+      dir : 'coverage/',
+      file : 'lcov.info'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
